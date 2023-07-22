7f8d11574a7a5a26cd99c4daeb8f052e2114de0a
unknown <somadinao@protonmail.com>
2023-07-22 20:43:21 +0100

    Changed password reset to post

--- client/README.md
+++
@@ -1,3 +0,0 @@
-# Getting Started with Create React App
-
-Social media accounts app created using React.
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,51 +1,31 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-const authController = require("../controllers/authController");
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router
-  .route("/")
-  .post(
-    authController.protect,
-    authController.restrictTo("admin"),
-    userController.createUser
-  )
-  .get(
-    authController.protect,
-    authController.restrictTo("admin"),
-    userController.getAllUsers
-  );
+  .route('/')
+  .post(authController.protect, authController.restrictTo('admin'), userController.createUser)
+  .get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers);
 
-router.post("/signUp", authController.signUp);
-router.post("/loginUser", authController.loginUser);
-router.post("/loginAdmin", authController.loginAdmin);
-router.post("/logout", authController.logout);
-router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword/:token", authController.resetPassowrd);
-router.patch(
-  "/updatePassword",
-  authController.protect,
-  authController.updatePassword
-);
+router.post('/signUp', authController.signUp);
+router.post('/loginUser', authController.loginUser);
+router.post('/loginAdmin', authController.loginAdmin);
+router.post('/logout', authController.logout);
+router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword/:token', authController.resetPassowrd);
+router.patch('/updatePassword', authController.protect, authController.updatePassword);
 
 router.use(authController.protect);
 
-router.patch(
-  "/block/:id",
-  authController.restrictTo("admin"),
-  userController.blockUser
-);
-router.patch(
-  "/unblock/:id",
-  authController.restrictTo("admin"),
-  userController.unblockUser
-);
+router.patch('/block/:id', authController.restrictTo('admin'), userController.blockUser);
+router.patch('/unblock/:id', authController.restrictTo('admin'), userController.unblockUser);
 
 router
-  .route("/:id")
+  .route('/:id')
   .patch(userController.updateUser)
   .get(userController.getOneUser)
-  .delete(authController.restrictTo("admin"), userController.deleteUser);
+  .delete(authController.restrictTo('admin'), userController.deleteUser);
 
 module.exports = router;
